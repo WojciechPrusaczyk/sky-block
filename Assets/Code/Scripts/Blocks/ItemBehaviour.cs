0d7c6f68be3dbcf54219386e9b8b0ed1778d1d35
WojciechPrusaczyk <woj.prusaczyk@gmail.com>
2025-10-05 16:15:16 +0200

    dodanie animacji unoszenia się itemów

--- Assets/Code/Scripts/Blocks/ItemBehaviour.cs
+++ Assets/Code/Scripts/Blocks/ItemBehaviour.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 public class ItemBehaviour : MonoBehaviour
 {
     public Item itemData;
+    public bool animateFloating = true;
+    public float maxBlockHeight = 0.15f;
+    public float floatSpeed = 1f;
+
+    private GameObject appearance;
+    private Vector3 baseLocalPosition;
 
     private void Awake()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        if(spriteRenderer && itemData)
-            spriteRenderer.sprite = itemData.WorldIcon;
+        appearance = gameObject.transform.Find("Appearance").gameObject;
+
+        if (appearance)
+            baseLocalPosition = appearance.transform.localPosition;
+
+        if(appearance && itemData)
+            appearance.gameObject.GetComponent<SpriteRenderer>().sprite = itemData.WorldIcon;
+    }
+
+    private void Update()
+    {
+        if (animateFloating && appearance)
+        {
+            float t = (Mathf.Sin(Time.time * floatSpeed + Mathf.PI * 0.5f) + 1f) * 0.5f;
+
+            float offsetY = t * maxBlockHeight;
+            appearance.transform.localPosition = new Vector3(
+                baseLocalPosition.x,
+                baseLocalPosition.y + offsetY,
+                baseLocalPosition.z
+            );
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
